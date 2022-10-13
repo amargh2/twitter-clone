17a82e6c7b55ccfedcf42030ba70585b9d743ee2
amargh2 <margherioa@gmail.com>
2022-10-12 23:16:37 -0500

    why is grid the right answer so often

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Post.module.scss'
 import {BiPoll} from 'react-icons/bi'
+import {AiOutlineBarChart} from 'react-icons/ai'
 import {useState} from 'react'
 import Image from 'next/image'
 export default function Post() {
@@ -7,23 +8,43 @@ export default function Post() {
   
   return (
     <div className={styles.postContainer}>
-        <Image src='/mesmall.jpg' width={100} height={100} alt='avatar'></Image>
-        <form action="/api/post">
-            <label for="message"></label>
-            <textarea class="form-control" 
-              name="message" 
-              onChange={event=>setFormValue(event.target.value)} 
-              id="message" 
-              rows="7" 
-              required 
-              minlength='10'  
-              placeholder="What's happening?">
-            </textarea>
-            <div>
-              <button className={styles.tweetbutton} disabled={formValue === '' ? true:false}>Tweet</button>
+          <div className={styles.avatar}>
+            <Image src='/mesmall.jpg' height={30} width={100} alt='avatar'></Image>
           </div>
-          </form>
-          
-      </div>
+
+            
+              <div className={styles.container}>
+                <form action='/api/post'>
+                    <label for="message"></label>
+                      <div className={styles.form}>
+                        <textarea className={styles.boxsizingBorder}
+                          name="message"
+                          onChange={event=>setFormValue(event.target.value)}
+                          id="message"
+                          rows="7"
+                          required
+                          minlength='10'
+                          placeholder="What's happening?">
+                        </textarea>
+                      </div>
+                      <div className={styles.buttonscontainer}>
+                    <div>
+                      <div className={styles.icon} onClick={()=>console.log('woah')}><AiOutlineBarChart size={24}></AiOutlineBarChart></div>
+                      <div className={styles.icon}><AiOutlineBarChart size={24}></AiOutlineBarChart></div>
+                      <div className={styles.icon}><AiOutlineBarChart size={24}></AiOutlineBarChart></div>
+                      <div className={styles.icon}><AiOutlineBarChart size={24}></AiOutlineBarChart></div>
+                      <div className={styles.icon}><AiOutlineBarChart size={24}></AiOutlineBarChart></div>
+                    </div>
+                    <div>
+                      <button
+                        className={styles.tweetbutton}
+                        disabled={formValue === '' ? true:false}>
+                        Tweet
+                      </button>
+                    </div>
+                  </div>
+                  </form>
+              </div>
+              </div>
   )
 }
