d92159b06c971f24e4b8b9c6cbf17f719a06576e
amargh2 <margherioa@gmail.com>
2022-10-28 21:56:53 -0500

    changed from image to a div with icon, looks better

--- pages/login.js
+++ pages/login.js
@@ -2,17 +2,16 @@ import LogInForm from "../components/LoginForm"
 import LoginFooter from "../components/loginfooter"
 import Image from "next/image"
 import styles from '../styles/Login.module.scss'
+import {
+  AiOutlineTwitter,
+} from 'react-icons/ai' 
 export default function LoginPage() {
   
   return (
     <div>
       <div className={styles.loginContainer}>
         <div className={styles.imageContainer}>
-          <Image
-            src='/twitter.png'
-            width={1300}
-            height={955}
-            alt='Twitter Sign In Page'/>
+          <AiOutlineTwitter className={styles.icon} size={500}/>
         </div>
         <div><LogInForm/></div>
       
