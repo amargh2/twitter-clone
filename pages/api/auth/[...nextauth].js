d1bf74c6c95f555c5d9bd9c46e9129a7295af919
amargh2 <margherioa@gmail.com>
2022-10-31 17:57:03 -0500

    changed middleware to redirect and settings to redirect to login page if unauthenticated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,9 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 import { unstable_getServerSession } from "next-auth/next";
 import res from "express/lib/response";
+
+//nextauth configuration options
+
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -9,6 +12,10 @@ export const authOptions = {
       clientSecret: process.env.clientSecret
     })
   ],
+  //changed default signin page to custom page ['/login']
+  pages: {
+    signIn: '/login'
+  },
   secret:process.env.NEXTAUTH_SECRET
 }
 
